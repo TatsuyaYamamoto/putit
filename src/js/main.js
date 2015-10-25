e081dd7102b52f967a383e6a201216ebd0340b9f
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-10-25 21:44:26 +0900

    sheet add機能追加。resizable, draggable機能をadd イベントハンドラ登録時に付与

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,39 @@
+var sheetTemplate = 	'<div class="sheet">' +
+							'<div class="head">' +
+								'<div class="row">' +
+									'<div class="remove">×</div>' +
+								'</div>' +
+							'</div>' +
+							'<div class="body">' +
+								'<div class="textarea" contenteditable="true"></div>' +
+							'</div>' +
+						'</div>';
+
 
 $(function() {
 
-	$(".sheet").find(".body").resizable();
 	$("#workspace").on("click", ".remove", function(){
 		$(this).parents(".sheet").remove();
 	})
 
-	$(".sheet").find(".head")
-		.on("mousedown", function(){
-			$(this).parent().draggable({disabled: false});
-		}).on("mouseup", function(){
-			$(this).parent().draggable({disabled: true});
-		})
+
+	$(".add").on("click", function(){
+		// シート追加
+		$("#workspace").prepend(sheetTemplate);
+
+		// 機能付与
+		$(".sheet").find(".body").resizable();
+
+		$(".sheet").find(".head")
+			.on("mousedown", function(){
+				$(this).parent().draggable({disabled: false,
+				stack: ".sheet"});
+			}).on("mouseup", function(){
+				$(this).parent().draggable({disabled: true});
+			})
+	})
+
+
 });
 
 
