d99ff4e1ee3a7bbec4ee08c3a92593094cb3ca04
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-10-28 21:21:49 +0900

    sheetListを追加

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,5 @@
-var sheetTemplate = '<div class="sheet">\
-						<div class="head">\
-							<div class="row">\
-								<div class="remove">×</div>\
-							</div>\
-						</div>\
-						<div class="body">\
-							<div class="textarea" contenteditable="true"></div>\
-						</div>\
-					</div>';
-
 
 $(function() {
-
-
 
 	// ドロワーメニューライブラリ読み込み
 	$(".drawer-handle").sidr({
@@ -21,14 +8,27 @@ $(function() {
 	});
 
 	$("#workspace").on("click", ".remove", function(){
-		$(this).parents(".sheet").remove();
+		var sheetId = $(this).parents(".sheet").attr("sheet-id");
+		$("#workspace").children('[sheet-id="' + sheetId + '"]').remove();
+		$("#sheetList").children('[sheet-id="' + sheetId + '"]').remove();
 	})
+	$("#sheetList").on("click", ".remove", function(){
+		var sheetId = $(this).parents(".sheetListItem").attr("sheet-id");
+		$("#workspace").children('[sheet-id="' + sheetId + '"]').remove();
+		$("#sheetList").children('[sheet-id="' + sheetId + '"]').remove();
+	})
+
 
 
 	$(".add").on("click", function(){
 
+		var sheetId = getUUID();
+
 		// シート追加
-		$("#workspace").prepend(sheetTemplate);
+		$("#workspace").append(getSheet(sheetId));
+
+		// シートリスト追加
+		$("#sheetList").prepend(getSheetListItem(sheetId));
 
 		// 機能付与
 		$(".sheet").find(".body").resizable();
@@ -67,5 +67,32 @@ $(function() {
 
 
 
-// UTIL function ---------------------------
+// Util function ---------------------------
 
+function getSheet(sheetId){
+
+	return	'<div class="sheet" sheet-id="' + sheetId + '">\
+				<div class="head">\
+					<div class="row">\
+						<div class="remove">×</div>\
+					</div>\
+				</div>\
+				<div class="body">\
+					<div class="textarea" contenteditable="true"></div>\
+				</div>\
+			</div>';
+}
+
+function getSheetListItem(sheetId){
+
+	return 	'<li class="sheetListItem" sheet-id="' + sheetId + '">\
+				<div class="remove">delete</div>\
+			</li>';
+}
+
+function getUUID(){
+    var S4 = function(){
+    	return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
