fc0a7e0b04ff613fa79400454b357b37d6daca82
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-11-02 10:39:07 +0900

    lock delete copy機能追加　イベントハンドラーリファクタリング

--- src/js/main.js
+++ src/js/main.js
@@ -9,74 +9,94 @@ $(function() {
 
 	// 削除ボタン
 	$("#workspace").on("click", ".delete", function(){
-		var sheetId = $(this).parents(".sheet").attr("sheet-id");
-		$("#workspace").children('[sheet-id="' + sheetId + '"]').remove();
-		$("#sheetList").children('[sheet-id="' + sheetId + '"]').remove();
+		var selectedSheetId = $(this).parents(".sheet").attr("sheet-id");
+		deleteSheet(selectedSheetId);
 	})
 
 	// 削除ボタン
 	$("#sheetList").on("click", ".delete", function(){
-		var sheetId = $(this).parents(".sheetListItem").attr("sheet-id");
-		$("#workspace").children('[sheet-id="' + sheetId + '"]').remove();
-		$("#sheetList").children('[sheet-id="' + sheetId + '"]').remove();
+		var selectedSheetId = $(this).parents(".sheetListItem").attr("sheet-id");
+		deleteSheet(selectedSheetId);
 	})
 
+	// コピーボタン
+	$("#sheetList").on("click", ".copy", function(){
+		var selectedSheetId = $(this).parents(".sheetListItem").attr("sheet-id");
+		copySheet(selectedSheetId);
+	})
+
+	// ロック
+	$("#workspace").on("click", ".lock", function(){
+		var selectedSheetId = $(this).parents(".sheet").attr("sheet-id");
+		lockSheet(selectedSheetId);
+	})
+	// アンロック
+	$("#workspace").on("click", ".unlock", function(){
+		var selectedSheetId = $(this).parents(".sheet").attr("sheet-id");
+		unlockSheet(selectedSheetId);
+	})
+
+
 	// シートリストアイテムのdeleteボタンを表示させる
-	$("#sidr").on("click", ".deleteAttache", function(){
-		$(".sheetListItem").append('<div class="delete">delete</div>');
+	$("#sidr").on("click", ".attacheMenu", function(){
+		$(".sheetListItem .copy").each(function(i, elem) {
+			$(elem).css('display', 'inline-block')
+		});
+		$(".sheetListItem .lock").each(function(i, elem) {
+			$(elem).css('display', 'inline-block')
+		});
+		$(".sheetListItem .delete").each(function(i, elem) {
+			$(elem).css('display', 'inline-block')
+		});
 	})
 
 	// シート内のテキストとsheetListItem内のテキストを同期させる
 	// 同期させる文字は"10文字以下""1行目のみ"
 	$("#workspace").on("keyup", ".textarea", function(){
-		var sheetId = $(this).parents(".sheet").attr("sheet-id");
-		var text = $(this).text();
-		if(text.length > 10) text = text.substr(0, 10) + "...";
-		$("#sheetList")
-			.find('[sheet-id="' + sheetId + '"] > .title')
-			.text(text);		
+		var selectedSheetId = $(this).parents(".sheet").attr("sheet-id");
+		updateSheet(selectedSheetId);
 	})
 
 	// シート追加ボタン
 	$(".add").on("click", function(){
-		addSheet(getUUID());
+		addSheet();
 	})
 
 
 	// 右クリック操作 -------------------
 	$("#workspace").contextmenu({
-    delegate: ".sheet",
-    menu: [
-        {title: "Copy", cmd: "copy", uiIcon: "ui-icon-copy"},
-        {title: "Delete", cmd: "delete", uiIcon: "ui-icon-trash"},
-        {title: "Lock", cmd: "lock", uiIcon: "ui-icon-pin-s"},
-    ],
-    select: function(event, ui) {
-        var selectedSheetId = ui.target.parents(".sheet").attr("sheet-id");
-        switch(ui.cmd){
-        	case "copy":
-        		var text = $('[sheet-id="' + selectedSheetId + '"]').find(".textarea").text();
-        		var addSheetId = getUUID()
-        		addSheet(addSheetId);
-        		$('[sheet-id="' + addSheetId + '"] >> .textarea').text(text);
-				if(text.length > 10){
-					text = text.substr(0, 10) + "...";
-				}
-				$("#sheetList")
-					.find('[sheet-id="' + addSheetId + '"] > .title')
-					.text(text);
-        		break;
+	    delegate: ".sheet",
+	    menu: [
+	        {title: "Copy", cmd: "copy", uiIcon: "ui-icon-copy"},
+	        {title: "Delete", cmd: "delete", uiIcon: "ui-icon-trash"},
+	        {title: "Lock/Unlock", cmd: "lock", uiIcon: "ui-icon-pin-s"},
+	    ],
+	    beforeOpen: function(event, ui) {
+	    	for ( var n in ui.target.cmd ) {
+	    		console.log(n)
 
-        	case "delete":
-				$("#workspace").children('[sheet-id="' + selectedSheetId + '"]').remove();
-				$("#sheetList").children('[sheet-id="' + selectedSheetId + '"]').remove();
-        		break;
+			}
+		},
+	    select: function(event, ui) {
+	        var selectedSheetId = ui.target.parents(".sheet").attr("sheet-id");
+	        switch(ui.cmd){
+	        	case "copy":
+					copySheet(selectedSheetId);
+	        		break;
 
-        	case "lock":
-        		break;
-        }
-    }
-});
+	        	case "delete":
+					deleteSheet(selectedSheetId);
+	        		break;
+
+	        	case "lock":
+	        		var $target = $("#workspace").children('[sheet-id="' + selectedSheetId + '"]').find(".lock");
+	        		$target.is('*')
+			        	?lockSheet(selectedSheetId)
+			        	:unlockSheet(selectedSheetId);
+	        		break;
+	        }
+	    }
+	});
 
 
 	// ドロワーメニュー -------------------
@@ -100,36 +120,89 @@ $(function() {
 
 // 共通機能 -----------------
 
-function addSheet(sheetId){
+function addSheet(){
 
+	var sheetId = getUUID()
 	// シート追加
 	$("#workspace").append(getSheet(sheetId));
-
 	// シートリスト追加
 	$("#sheetList").append(getSheetListItem(sheetId));
-
 	// 機能付与
-	$(".sheet").find(".body").resizable();
+	$(".sheet .body").resizable({ autoHide : true });
 
-	$(".sheet").find(".head")
-		.on("mousedown", function(){
-			$(this).parent().draggable({disabled: false,
-			stack: ".sheet"});
-		}).on("mouseup", function(){
-			$(this).parent().draggable({disabled: true});
-		})
+	$(".sheet").draggable({
+					disabled: false,
+					handle : '.head',
+					scroll : false,
+					stack : ".sheet"
+				})
+	return sheetId;
+}
+
+function lockSheet(sheetId){
+
+	$sheet = $("#workspace").children('[sheet-id="' + sheetId + '"]');
+
+	$sheet.find(".lock")
+		.removeClass("ui-icon-pin-w")
+		.addClass("ui-icon-pin-s")
+		.removeClass("lock")
+		.addClass("unlock")
+
+	$sheet.find(".delete").hide();
+	$sheet.find(".textarea").attr("contenteditable","false");
+	$sheet.find(".body").resizable('disable');
+	$sheet.draggable('disable');
+}
+function unlockSheet(sheetId){
+
+	$sheet = $("#workspace").children('[sheet-id="' + sheetId + '"]');
+
+	$sheet.find(".unlock")	
+		.removeClass("ui-icon-pin-s")
+		.addClass("ui-icon-pin-w")
+		.removeClass("unlock")
+		.addClass("lock")
+
+	$sheet.find(".delete").show();
+
+	$sheet.find(".textarea").attr("contenteditable","true")
+	$sheet.find(".body").resizable('enable');
+	$sheet.draggable('enable');
+}
+
+
+function deleteSheet(sheetId){
+	$("#workspace").children('[sheet-id="' + sheetId + '"]').remove();
+	$("#sheetList").children('[sheet-id="' + sheetId + '"]').remove();
+}
+
+function copySheet(sheetId){
+	var text = $('[sheet-id="' + sheetId + '"]').find(".textarea").text();
+	var addSheetId = addSheet();
+	$('[sheet-id="' + addSheetId + '"] >> .textarea').text(text);
+	if(text.length > 10) text = text.substr(0, 10) + "...";
+	$("#sheetList")
+		.find('[sheet-id="' + addSheetId + '"] > .title')
+		.text(text);
+}
+
+function updateSheet(sheetId){
+
+	var text = $('[sheet-id="' + sheetId + '"]').find(".textarea").text();
+	if(text.length > 10) text = text.substr(0, 10) + "...";
+	$("#sheetList")
+		.find('[sheet-id="' + sheetId + '"] > .title')
+		.text(text);
 }
 
 // Util function ---------------------------
-
 function getSheet(sheetId){
 
 	return	'<div class="sheet" sheet-id="' + sheetId + '">\
 				<div class="head">\
-					<div class="row">\
-						<div class="delete ui-icon ui-icon-closethick"></div>\
-						<div class="lock ui-icon ui-icon-pin-w">a</div>\
-					</div>\
+					<div class="delete icon ui-icon ui-icon-closethick"></div>\
+					<div class="lock icon ui-icon ui-icon-pin-w">a</div>\
 				</div>\
 				<div class="body">\
 					<div class="textarea" contenteditable="true"></div>\
@@ -141,6 +214,9 @@ function getSheetListItem(sheetId){
 
 	return 	'<li class="sheetListItem" sheet-id="' + sheetId + '">\
 				<div class="title"></div>\
+				<div class="copy">Copy</div>\
+				<div class="lock">Lock</div>\
+				<div class="delete">Delete</div>\
 			</li>';
 }
 
